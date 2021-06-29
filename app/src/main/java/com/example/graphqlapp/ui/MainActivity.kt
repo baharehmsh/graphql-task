07a000c2054053b0b93bbe9971ec8840e6b1a43d
Bahareh Bahadori <bahareh@baharehs-mbp.lan>
2021-06-29 10:49:37 +0200

    Added AllpostsFragment

--- app/src/main/java/com/example/graphqlapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/graphqlapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.graphqlapp
+package com.example.graphqlapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.graphqlapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/graphqlapp/ui/allposts/AllPostsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.graphqlapp.ui.allposts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.graphqlapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AllPostsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_all_posts, container, false)
+    }
+
+
+}
